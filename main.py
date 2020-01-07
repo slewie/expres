9e2d1b73fc9746b20d1ae34c28eee73540304304
slewie <57148398+slewie@users.noreply.github.com>
2020-01-07 20:14:13 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,143 +1,35 @@
-import sys
-
-from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem, QWidget
-import sqlite3
-from addEditCoffeeForm import Ui_Form
-from mainui import Ui_MainWindow
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QWidget, QTableWidgetItem, QTableWidget
+from PyQt5 import uic
+import sys, sqlite3
 
 
-class Main(QMainWindow, Ui_MainWindow):
-
+class Maim(QWidget):
     def __init__(self):
-        super(Main, self).__init__()
-        self.setupUi(self)
-
-        # self.tableWidget = QTableWidget()
-
-        self.initUI()
-
-    def initUI(self):
-        self.pushButton.clicked.connect(self.addWindow)
-        self.table_init()
-        self.addAlltotable()
-
-    def addAlltotable(self):
-        self.tableWidget.setRowCount(0)
-        con = sqlite3.connect("data\coffee.sqlite")
-        cur = con.cursor()
-        data = cur.execute("SELECT * FROM coffee WHERE id>0").fetchall()
-        for row, cof in enumerate(data):
-            self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
-            for col, d in enumerate(cof):
-                if col == 3:
-                    if str(d) == '1':
-                        d = 'молотый'
-                    elif str(d) == '2':
-                        d = 'в зернах'
-                if d is not None:
-                    self.tableWidget.setItem(row, col, QTableWidgetItem(str(d)))
-                else:
-                    self.tableWidget.setItem(row, col, QTableWidgetItem(''))
-
-        self.tableWidget.resizeColumnsToContents()
-
-    def table_init(self):
-        self.tableWidget.setColumnCount(7)
-        self.tableWidget.setHorizontalHeaderLabels(
-            ['ID', 'название сорта', 'степень обжарки', 'молотый/в зернах', 'описание вкуса', 'цена', 'объем упаковки'])
-
-        self.tableWidget.resizeColumnsToContents()
-
-    def addWindow(self):
-        self.addw = AddWindow(self)
-        self.addw.show()
-
-
-class AddWindow(QWidget, Ui_Form):
-    def __init__(self, mainW):
-        self.MainW = mainW
         super().__init__()
-        self.setupUi(self)
+        uic.loadUi('main.ui', self)
         self.initUI()
 
     def initUI(self):
-        self.save_btn.clicked.connect(self.save)
-        self.add_btn.clicked.connect(self.add)
-        self.table_init()
-        self.addAlltotable()
-
-    def addAlltotable(self):
-        con = sqlite3.connect("data\coffee.sqlite")
-        cur = con.cursor()
-        data = cur.execute("SELECT * FROM coffee WHERE id>0").fetchall()
-        for row, cof in enumerate(data):
-            self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
-            for col, d in enumerate(cof[1:]):
-                if col == 2:
-                    if str(d) == '1':
-                        d = 'молотый'
-                    elif str(d) == '2':
-                        d = 'в зернах'
-                if d is not None:
-                    self.tableWidget.setItem(row, col, QTableWidgetItem(str(d)))
-                else:
-                    self.tableWidget.setItem(row, col, QTableWidgetItem(''))
-
-        self.tableWidget.resizeColumnsToContents()
-
-    def table_init(self):
-        self.tableWidget.setColumnCount(6)
-        self.tableWidget.setHorizontalHeaderLabels(
-            ['название сорта', 'степень обжарки', 'молотый/в зернах', 'описание вкуса', 'цена', 'объем упаковки'])
-
-        self.tableWidget.resizeColumnsToContents()
-
-    def save(self):
-        #   self.tableWidget = QTableWidget(self)
-        titles = ['name', 'degree', 'type', 'taste', 'price', 'volume']
-        self.cleanDB()
-
-        for row in range(self.tableWidget.rowCount() + 1):
-            data = {}
-            for col in range(self.tableWidget.columnCount() + 1):
-                item = self.tableWidget.item(row, col)
-                if item is not None and item.text() != '':
-                    data[titles[col]] = item.text()
-
-            keys, vals = list(data.keys()), list(data.values())
-            for i in range(len(keys)):
-                if keys[i] in ('name', 'taste', 'degree', 'volume'):
-                    vals[i] = "'" + vals[i] + "'"
-                if keys[i] == 'type':
-                    if vals[i] == 'молотый':
-                        vals[i] = '1'
-                    elif vals[i] == 'в зернах':
-                        vals[i] = '2'
-
-            if keys != []:
-                s = f"INSERT INTO coffee({', '.join(keys)}) VALUES ({', '.join(vals)})"
-                self.toDB(s)
-
-        self.MainW.addAlltotable()
-        self.close()
-
-    def cleanDB(self):
-        con = sqlite3.connect("data\coffee.sqlite")
-        with con:
-            cur = con.cursor()
-            cur.execute("DELETE from coffee WHERE id>0")
-
-    def toDB(self, sql_string):
-        con = sqlite3.connect("data\coffee.sqlite")
-        cur = con.cursor()
-        with con:
-            cur.execute(sql_string)
-
-    def add(self):
-        self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setHorizontalHeaderLabels(["ID", "название сорта", "степень обжарки", "молотый/в зернах", "описание вкуса", "цена", "объем упаковки"])
+        self.data = sqlite3.connect('coffee.sqlite')
+        cur = self.data.cursor()
+        lst = cur.execute("""SELECT * FROM coffee""").fetchall()
+        self.tableWidget.setRowCount(len(lst))
+        for i in range(len(lst)):
+            self.tableWidget.setItem(i, 0, QTableWidgetItem(str(lst[i][0])))
+            self.tableWidget.setItem(i, 1, QTableWidgetItem(lst[i][1]))
+            self.tableWidget.setItem(i, 2, QTableWidgetItem(lst[i][2]))
+            if lst[i][3] is not None:
+                self.tableWidget.setItem(i, 3, QTableWidgetItem(cur.execute(f"""SELECT title FROM type WHERE id = {lst[i][3]}""").fetchone()[0]))
+            self.tableWidget.setItem(i, 4, QTableWidgetItem(lst[i][4]))
+            self.tableWidget.setItem(i, 5, QTableWidgetItem(lst[i][5]))
+            self.tableWidget.setItem(i, 6, QTableWidgetItem(lst[i][6]))
 
 
-app = QApplication(sys.argv)
-w = Main()
-w.show()
-sys.exit(app.exec())
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    form = Maim()
+    form.show()
+    sys.exit(app.exec_())
